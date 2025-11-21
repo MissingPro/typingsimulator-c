f98a63540fb3ddfcb9532dad8734f86f90d93750
MissingPro <missingpromc@Outlook.com>
2025-11-21 17:41:57 -0500

    it's a c file

---
+++ typingsimulator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  system("title Typing Simulator v1.0.0");
+  printf("Typing Simulator v1.0.0\n");
+  printf("Made in C!\n");
+  while(1) {
+    getchar();
+  }
+}
